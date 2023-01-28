340225f06021b0a000951ad92637671344c575a6
Georgii Sokolov <geospbarch@gmail.com>
2023-01-29 00:03:10 +0100

    Signed-off-by: Georgii Sokolov <geospbarch@gmail.com>

---
+++ 3dPlayground-Core/src/3dPlayground-Core/utils/test.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "3dPlayground-Core\utils\test.hpp"
+
+void test()
+{
+    printf("Test ok!");
+}
---
+++ Player/src/main.cpp
@@ -0,0 +1,7 @@
+#include "3dPlayground-Core\utils\test.hpp"
+
+int main()
+{
+    test();
+    return 0;
+}
